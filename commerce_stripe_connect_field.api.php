c4beba632c6f6a8536793f05165f09b10f79db27
Nicolò Caruso <nicolo@istos.it>
2017-06-12 17:48:08 +0200

    Add ‘hook_commerce_stripe_order_charge_alter’ and ‘hook_commerce_stripe_order_options_alter’

---
+++ commerce_stripe_connect_field.api.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * This code is never called, it's just information about to use the hooks.
+ */
+
+function hook_commerce_stripe_order_description_alter(&$description, $order) {
+  // No example.
+}
+
+function hook_commerce_stripe_order_charge_alter(&$charge, $order) {
+  // No example.
+}
+
+function hook_commerce_stripe_order_options_alter(&$options, $order) {
+  // No example.
+}
